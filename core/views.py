941756af482964eb87917705994c4d15165c292a
Fabrício Chiaradia <fabricioxc@gmail.com>
2021-09-20 23:29:37 -0300

    Inclusão da rota operacoes para retornar + - * /

--- core/views.py
+++ core/views.py
@@ -9,3 +9,21 @@ def hello(request, nome, idade):
     else:
         msg = 'maior de idade'
     return HttpResponse(f'<h1>Hello {nome}! Você é {msg}</h1>')
+
+def operacoes(request, operacao, a, b):
+    msg = ''
+    total = 0
+    if operacao not in ['soma', 'multiplicacao', 'divisao', 'subtracao']:
+        return HttpResponse(f'<h1>A operação {operacao} não existe nesta aplicação.</h1>')
+
+    if operacao == 'soma':
+        total = a + b
+    elif operacao == 'subtracao':
+        total = a - b
+    elif operacao == 'multiplicacao':
+        total = a * b
+    else:
+        total = a / b
+    
+    return HttpResponse(f'<h1>A {operacao} entre {a} e {b} é {total}</h1>')
+
